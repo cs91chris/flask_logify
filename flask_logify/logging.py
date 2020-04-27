6bea114c96d86fe2753f13dfc09cbe6f7d8ed73e
cs91chris <cs91chris@voidbrain.me>
2020-04-27 20:23:32 +0200

    enable force debug with flask app conf

--- flask_logify/logging.py
+++ flask_logify/logging.py
@@ -38,22 +38,28 @@ class FlaskLogging:
         app.extensions['logging'] = self
 
         app.config.setdefault('LOG_APP_NAME', 'flask')
-        app.config.setdefault('LOG_LOGGER_NAME', 'flask')
+        app.config.setdefault('LOG_LOGGER_NAME', 'flask-development')
 
         app.before_request_funcs.setdefault(None, []).append(hook_log_request)
         app.after_request_funcs.setdefault(None, []).append(hook_log_response)
 
         if 'LOG_FILE_CONF' in app.config:
-            with open(app.config['LOG_FILE_CONF']) as f:
-                try:
-                    self._conf = yaml.safe_load(f)
-                except (ParserError, ScannerError) as exc:
-                    print(exc, file=sys.stderr)
-                    sys.exit(1)
+            try:
+                with open(app.config['LOG_FILE_CONF']) as f:
+                    try:
+                        self._conf = yaml.safe_load(f)
+                    except (ParserError, ScannerError) as exc:
+                        print(exc, file=sys.stderr)
+                        sys.exit(1)
+            except OSError as exc:
+                app.logger.exception(exc, stack_info=False)
 
+            self._conf.update(kwargs)
+            logging.config.dictConfig(self._conf)
             app.logger = logging.getLogger(app.config['LOG_LOGGER_NAME'])
         else:
             app.logger.warning("No 'LOG_FILE_CONF' provided using default configuration")
 
-        self._conf.update(kwargs)
-        logging.config.dictConfig(self._conf)
+        if app.config.get('DEBUG'):
+            # overwrites file log configuration in order to force the debug level
+            app.logger.setLevel('DEBUG')

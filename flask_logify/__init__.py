2bdf710e263396947af16f9e5691bdde0defb0dc
cs91chris <cs91chris@voidbrain.me>
2020-04-27 22:29:03 +0200

    add params for custom logs functions

--- flask_logify/__init__.py
+++ flask_logify/__init__.py
@@ -1,4 +1,4 @@
 from .version import *
-
 from .logging import FlaskLogging
 from .handler import FlaskSysLogHandler
+from .hooks import hook_log_request, hook_log_response
--- flask_logify/logging.py
+++ flask_logify/logging.py
@@ -5,19 +5,22 @@ import yaml
 from yaml.parser import ParserError
 from yaml.scanner import ScannerError
 
-from flask_logify.hooks import hook_log_request, hook_log_response
+from .hooks import hook_log_request, hook_log_response
 
 
 class FlaskLogging:
-    def __init__(self, app=None):
+    def __init__(self, app=None, **kwargs):
         """
 
         :param app:
         """
         self._conf = {}
+        self._lock = False
+        self._log_request = None
+        self._log_response = None
 
         if app is not None:
-            self.init_app(app)
+            self.init_app(app, **kwargs)
 
     @property
     def conf(self):
@@ -27,12 +30,17 @@ class FlaskLogging:
         """
         return self._conf
 
-    def init_app(self, app, **kwargs):
+    def init_app(self, app, log_req=None, log_res=None, **kwargs):
         """
 
         :param app: Flask app instance
+        :param log_req: custom function that logs request
+        :param log_res: custom function that logs response
         :param kwargs: extra logging configuration
         """
+        self._log_request = log_req or hook_log_request
+        self._log_response = log_res or hook_log_response
+
         if not hasattr(app, 'extensions'):
             app.extensions = dict()
         app.extensions['logging'] = self
@@ -40,8 +48,8 @@ class FlaskLogging:
         app.config.setdefault('LOG_APP_NAME', 'flask')
         app.config.setdefault('LOG_LOGGER_NAME', 'flask-development')
 
-        app.before_request_funcs.setdefault(None, []).append(hook_log_request)
-        app.after_request_funcs.setdefault(None, []).append(hook_log_response)
+        app.before_request_funcs.setdefault(None, []).append(self._log_request)
+        app.after_request_funcs.setdefault(None, []).append(self._log_response)
 
         if 'LOG_FILE_CONF' in app.config:
             try:

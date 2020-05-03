b1260fad56b4f4a2125f7cbdc13a0914e14ff9a2
cs91chris <cs91chris@voidbrain.me>
2020-05-03 19:11:58 +0200

    refactored hooks

--- flask_logify/hooks.py
+++ flask_logify/hooks.py
@@ -1,48 +1,109 @@
 from flask import request
-from flask import current_app as app
+from flask import current_app as cap
 
 
-def hook_log_request():
+def get_remote_addr():
     """
 
+    :return:
     """
     if request.environ.get('HTTP_X_FORWARDED_FOR'):
         request.environ['REMOTE_ADDR'] = request.environ['HTTP_X_FORWARDED_FOR']
     elif request.environ.get('HTTP_X_REAL_IP'):
         request.environ['REMOTE_ADDR'] = request.environ['HTTP_X_REAL_IP']
 
-    remote_address = request.environ['REMOTE_ADDR']
-    app.logger.info(
-        "INCOMING REQUEST: %s %s %s %s",
-        remote_address,
-        request.method,
-        request.scheme,
-        request.full_path
+    return request.environ['REMOTE_ADDR']
+
+
+def dump_headers(hdr, force=False):
+    """
+    dumps http headers: useful for logging
+
+    :param force:
+    :param hdr: headers' dictionary
+    :return: string representation of headers
+            k1: v1
+            k2: v2
+    """
+    if force or cap.config['DEBUG'] is True:
+        return '\n'.join('{}: {}'.format(k, v) for k, v in hdr.items())
+    else:
+        return 'headers not dumped'
+
+
+def dump_body(r, force=False):
+    """
+    dump http body as plain text
+
+    :param r:
+    :param force:
+    :return:
+    """
+    if force or cap.config['DEBUG'] is True:
+        return r.get_data(as_text=True) or 'empty data'
+    else:
+        return 'body not dumped: app not in DEBUG mode'
+
+
+def dump_request(force=False):
+    """
+
+    :param force:
+    :return:
+    """
+    remote_address = get_remote_addr()
+
+    return "{addr} {method} {scheme} {path}\n\n{headers}\n\n{body}\n".format(
+        addr=remote_address,
+        method=request.method,
+        scheme=request.scheme,
+        path=request.full_path,
+        headers=dump_headers(request.headers, force),
+        body=dump_body(request, force)
     )
 
-    app.logger.debug("REQUEST HEADERS\n%s", request.headers)
 
-    data = request.get_data(as_text=True)
-    if data:
-        app.logger.debug("REQUEST BODY\n{}".format(data))
+def dump_response(response, force=False):
+    """
+
+    :param response:
+    :param force:
+    :return:
+    """
+    code = int(response.status_code / 100)
+    if code in (1, 2, 3):
+        level = 'SUCCESS'
+    elif code == 4:
+        level = 'WARNING'
+    elif code == 5:
+        level = 'ERROR'
+    else:
+        level = 'UNKNOWN'
+
+    return "({level}) STATUS {status}\n\n{headers}\n\n{body}\n".format(
+        level=level,
+        status=response.status,
+        headers=dump_headers(response.headers, force),
+        body=dump_body(response, force)
+    )
+
+
+def hook_log_request():
+    """
+
+    """
+    cap.logger.info("INCOMING REQUEST: {}".format(
+        dump_request()
+    ))
 
 
 def hook_log_response(response):
     """
 
     :param response:
-    :return:
     """
-    code = int(response.status_code / 100)
-    log = app.logger.debug
-
-    if code in (1, 2, 3):
-        log = app.logger.info
-    if code == 4:
-        log = app.logger.warning
-    if code == 5:
-        log = app.logger.error
-
-    log("RESPONSE STATUS: %s", response.status)
-    app.logger.debug("RESPONSE HEADERS\n%s", response.headers)
+    cap.logger.debug("RESPONSE at {} {}".format(
+        request.path,
+        dump_response(response)
+    ))
     return response

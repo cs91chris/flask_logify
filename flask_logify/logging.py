26dfbef211d6a8fb1edb30b603408814c350d8d2
cs91chris <cs91chris@voidbrain.me>
2020-06-05 16:30:30 +0200

    refactored: moved hooks inside class

--- flask_logify/logging.py
+++ flask_logify/logging.py
@@ -1,13 +1,12 @@
-import sys
 import json
 import logging.config
+import sys
 from json import JSONDecodeError
 
 import yaml
+from flask import current_app as cap, request
 from yaml.parser import ParserError
 from yaml.scanner import ScannerError
-
-from .hooks import hook_log_request, hook_log_response
 
 
 class FlaskLogging:
@@ -17,8 +16,6 @@ class FlaskLogging:
         :param app:
         """
         self._conf = {}
-        self._log_request = None
-        self._log_response = None
 
         if app is not None:
             self.init_app(app, **kwargs)
@@ -31,27 +28,21 @@ class FlaskLogging:
         """
         return self._conf
 
-    def init_app(self, app, log_req=hook_log_request, log_res=hook_log_response, **kwargs):
+    def init_app(self, app, **kwargs):
         """
 
         :param app: Flask app instance
-        :param log_req: custom function that logs request
-        :param log_res: custom function that logs response
         :param kwargs: extra logging configuration
         """
         if not hasattr(app, 'extensions'):
             app.extensions = dict()
         app.extensions['logify'] = self
 
-        app.config.setdefault('LOG_FORCE_DUMP', app.config.get('DEBUG'))
-        app.config.setdefault('LOG_APP_NAME', 'flask')
-        app.config.setdefault('LOG_LOGGER_NAME', '{}-{}'.format(
-            app.config['LOG_APP_NAME'],
-            app.config.get('FLASK_ENV') or 'development'
-        ))
+        self.set_default_config(app)
 
-        app.before_request_funcs.setdefault(None, []).append(log_req)
-        app.after_request_funcs.setdefault(None, []).append(log_res)
+        if not app.config['LOG_DISABLE_HOOKS']:
+            app.before_request_funcs.setdefault(None, []).append(self.log_request)
+            app.after_request_funcs.setdefault(None, []).append(self.log_response)
 
         if 'LOG_FILE_CONF' in app.config:
             try:
@@ -72,7 +63,7 @@ class FlaskLogging:
 
         if app.config.get('DEBUG') is True:
             app.logger.setLevel('DEBUG')
-            self.set_werkzeug_handler(app.logger.handlers)
+            self.set_werkzeug_handlers(app.logger.handlers)
 
     def conf_from_file(self, filename):
         """
@@ -90,8 +81,133 @@ class FlaskLogging:
                     print(jex, file=sys.stderr)
                     sys.exit(1)
 
+    def log_request(self):
+        """
+
+        """
+        body = None
+        headers = None
+        skip = cap.config['LOG_SKIP_DUMP']
+        hdr = cap.config['LOG_REQ_HEADERS']
+
+        if '{headers}' in cap.config['LOG_RESP_FORMAT'] and not skip:
+            headers = self.dump_headers(request.headers, hdr)
+
+        if '{body}' in cap.config['LOG_RESP_FORMAT'] and not skip:
+            body = self.dump_body(request)
+
+        request_dumped = cap.config['LOG_REQ_FORMAT'].format(
+            address=self.get_remote_address(),
+            method=request.method,
+            scheme=request.scheme,
+            path=request.full_path,
+            headers=headers,
+            body=body
+        )
+
+        cap.logger.info("INCOMING REQUEST: {}".format(request_dumped))
+
+    def log_response(self, response):
+        """
+
+        :param response: Response object
+        """
+        code = int(response.status_code / 100)
+        if code in (1, 2, 3):
+            level = 'SUCCESS'
+        elif code == 4:
+            level = 'WARNING'
+        elif code == 5:
+            level = 'ERROR'
+        else:
+            level = 'UNKNOWN'
+
+        body = None
+        headers = None
+        skip = cap.config['LOG_SKIP_DUMP']
+        hdr = cap.config['LOG_REQ_HEADERS']
+
+        if '{headers}' in cap.config['LOG_RESP_FORMAT'] and not skip:
+            headers = self.dump_headers(response.headers, hdr)
+
+        if '{body}' in cap.config['LOG_RESP_FORMAT'] and not skip:
+            body = self.dump_body(response)
+
+        response_dumped = cap.config['LOG_RESP_FORMAT'].format(
+            level=level,
+            status=response.status,
+            headers=headers,
+            body=body
+        )
+
+        cap.logger.debug("OUTGOING RESPONSE at {} {}".format(request.path, response_dumped))
+        return response
+
     @staticmethod
-    def set_werkzeug_handler(handlers):
+    def dump_headers(hdr, only=()):
+        """
+        dumps http headers
+
+        :param hdr: headers' dictionary
+        :param only: list of headers key to dump
+        :return: string representation of headers
+                k1: v1
+                k2: v2
+        """
+        def dump(h):
+            return '\n'.join('{}: {}'.format(k, v) for k, v in h.items())
+
+        if only:
+            return dump({k: hdr[k] for k in only if k in hdr})
+
+        return dump(hdr)
+
+    @staticmethod
+    def dump_body(r):
+        """
+        dump http body as plain text
+
+        :param r: Response object
+        :return:
+        """
+        try:
+            return r.get_data(as_text=True) or 'empty data'
+        except UnicodeError:
+            return 'body not dumped: invalid encoding or binary'
+
+    @staticmethod
+    def set_default_config(app):
+        """
+
+        param app: Flask app instance
+        """
+        app.config.setdefault('LOG_REQ_HEADERS', [])
+        app.config.setdefault('LOG_RESP_HEADERS', [])
+        app.config.setdefault('LOG_DISABLE_HOOKS', False)
+        app.config.setdefault('LOG_SKIP_DUMP', not app.config.get('DEBUG'))
+        app.config.setdefault('LOG_RESP_FORMAT', "{level} STATUS {status}\n{headers}\n\n{body}\n")
+        app.config.setdefault('LOG_REQ_FORMAT', "{address} {method} {scheme} {path}\n{headers}\n\n{body}\n")
+        app.config.setdefault('LOG_APP_NAME', 'flask')
+        app.config.setdefault('LOG_LOGGER_NAME', '{}-{}'.format(
+            app.config['LOG_APP_NAME'],
+            app.config.get('FLASK_ENV') or 'development'
+        ))
+
+    @staticmethod
+    def get_remote_address():
+        """
+
+        :return: client ip address
+        """
+        if request.environ.get('HTTP_X_FORWARDED_FOR'):
+            request.environ['REMOTE_ADDR'] = request.environ['HTTP_X_FORWARDED_FOR']
+        elif request.environ.get('HTTP_X_REAL_IP'):
+            request.environ['REMOTE_ADDR'] = request.environ['HTTP_X_REAL_IP']
+
+        return request.environ['REMOTE_ADDR']
+
+    @staticmethod
+    def set_werkzeug_handlers(handlers):
         """
 
         :param handlers:

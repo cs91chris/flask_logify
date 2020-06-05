a5f81592607f1c12cf623dd4cae59848aae31d40
cs91chris <cs91chris@voidbrain.me>
2020-06-05 23:39:21 +0200

    update example

--- flask_logify/example/test.py
+++ flask_logify/example/test.py
@@ -1,20 +1,27 @@
 from flask import Flask
 
-from flask_logify import FlaskLogging, log_disabled_by_path
+from flask_logify import FlaskLogging
+from flask_logify.builders import LogJSONBuilder
+from flask_logify.filters import PathFilter
 
 
 def create_app():
     app = Flask(__name__)
     app.config['DEBUG'] = True
     app.config['LOG_FILE_CONF'] = 'log.yaml'
+    app.config['LOG_REQ_HEADERS'] = ['User-Agent']
     app.config['LOG_LOGGER_NAME'] = 'flask-development'
     logging = FlaskLogging()
 
     with app.app_context():
-        logging.init_app(app)
+        logging.init_app(app, builder=LogJSONBuilder())
+
+    @app.route('/test')
+    def test():
+        return {"test": "test"}
 
     @app.route('/health')
-    @log_disabled_by_path()
+    @logging.disabled(PathFilter, path='/health')
     def health():
         return 'status OK'
 

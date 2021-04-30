f1ed67012334a14124f06609028c64e2b3cb6cf1
cs91chris <cs91chris@voidbrain.me>
2021-04-30 03:03:41 +0200

    add logging queue handler

--- flask_logify/handlers.py
+++ flask_logify/handlers.py
@@ -1,4 +1,8 @@
-from logging.handlers import SysLogHandler
+import atexit
+# noinspection PyUnresolvedReferences
+from logging.config import ConvertingDict, ConvertingList, valid_ident
+from logging.handlers import QueueHandler as BaseQueueHandler, QueueListener, SysLogHandler
+from queue import Queue
 
 from flask import current_app as cap
 
@@ -21,3 +25,55 @@ class FlaskSysLogHandler(SysLogHandler):
         priority = self.encodePriority(self.facility, self.mapPriority(record.levelname))
         record.ident = f"{self._app_name}[{priority}]:"
         super(FlaskSysLogHandler, self).emit(record)
+
+
+class QueueHandler(BaseQueueHandler):
+    """
+    From: https://rob-blackbourn.medium.com/how-to-use-python-logging-queuehandler-with-dictconfig-1e8b1284e27a
+    """
+    def __init__(self, handlers, respect_handler_level=False, auto_run=True, queue=None):
+        # noinspection PyTypeChecker
+        queue = self._resolve_queue(queue or Queue(-1))
+        super().__init__(queue)
+        handlers = self._resolve_handlers(handlers)
+        # noinspection PyUnresolvedReferences
+        self._listener = QueueListener(
+            self.queue, *handlers,
+            respect_handler_level=respect_handler_level
+        )
+        if auto_run:
+            self.start()
+            atexit.register(self.stop)
+
+    def start(self):
+        self._listener.start()
+
+    def stop(self):
+        self._listener.stop()
+
+    def emit(self, record):
+        return super().emit(record)
+
+    @staticmethod
+    def _resolve_handlers(h):
+        if not isinstance(h, ConvertingList):
+            return h
+        return [h[i] for i in range(len(h))]
+
+    @staticmethod
+    def _resolve_queue(q):
+        if not isinstance(q, ConvertingDict):
+            return q
+        if '__resolved_value__' in q:
+            return q['__resolved_value__']
+
+        cname = q.pop('class')
+        klass = q.configurator.resolve(cname)
+        props = q.pop('.', None) or {}
+        kwargs = {k: q[k] for k in q if valid_ident(k)}
+        result = klass(**kwargs)
+        for name, value in props.items():
+            setattr(result, name, value)
+
+        q['__resolved_value__'] = result
+        return result

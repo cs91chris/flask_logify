e199e72b43ebe65e88141525b47003d0a7ae0c13
cs91chris <cs91chris@voidbrain.me>
2021-06-27 02:05:49 +0200

    new OnlyPathsFilter filter

--- flask_logify/filters.py
+++ flask_logify/filters.py
@@ -5,7 +5,7 @@ class FilterByArgs(logging.Filter):
     def __init__(self, name='', *args):
         """
 
-        :param path:
+        :param name:
         :param args:
         """
         super().__init__(name)
@@ -38,3 +38,31 @@ class PathFilter(FilterByArgs):
             f"{path} ",
             f"{path}?"
         )
+
+
+class OnlyPathsFilter(logging.Filter):
+    def __init__(self, name='', paths=()):
+        """
+
+        :param name:
+        :param paths:
+        """
+        super().__init__(name)
+        self._only = paths
+
+    def filter(self, record):
+        """
+
+        :param record:
+        :return: False if message must be filtered
+        """
+        if not hasattr(record, 'path'):
+            return True
+
+        # noinspection PyUnresolvedReferences
+        path = record.path
+        for p in self._only:
+            if p == path:
+                return True
+
+        return False
